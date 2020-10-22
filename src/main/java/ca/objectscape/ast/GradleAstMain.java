17802c55f9b6006972381ad6d5632573502287c1
Eduard Tita <etita@sonatype.com>
2020-10-22 11:51:15 -0400

    Small improvements

--- src/main/java/ca/objectscape/ast/GradleAstMain.java
+++ src/main/java/ca/objectscape/ast/GradleAstMain.java
@@ -13,12 +13,12 @@ import org.codehaus.groovy.control.MultipleCompilationErrorsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AstMain
+public class GradleAstMain
 {
-  private static final Logger log = LoggerFactory.getLogger(AstMain.class);
+  private static final Logger log = LoggerFactory.getLogger(GradleAstMain.class);
 
   public static void main(String[] args) throws IOException {
-    final String buildFile = "src/main/resources/build.gradle";
+    final String buildFile = "src/main/resources/groovy/build.gradle";
     final String text = readFile(buildFile, Charset.defaultCharset());
 
     final List<ASTNode> astNodes = build(text);
